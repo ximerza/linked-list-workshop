8ee0eb06870c94a8424f2e09650b3b50bbcc9440
Jesus Hincapie <jahlon@gmail.com>
2025-01-17 13:02:14 -0500

    Add comment to implement solution

--- datastruct/exercises/solution_3.py
+++ datastruct/exercises/solution_3.py
@@ -2,34 +2,5 @@ from datastruct.classes.lists import LinkedList
 
 
 def sort_list_by_insertion(linked_list: LinkedList[int]) -> LinkedList[int]:
-    """
-    Sort a linked list using insertion sort. In the same list (Do not create another)
-    :param linked_list:
-    :return:
-    """
-    if linked_list.is_empty():
-        return linked_list
-
-    current = linked_list.head
-    while current:
-        next_node = current.next
-        if next_node and current.data > next_node.data:
-            previous = None
-            temp = linked_list.head
-            while temp and temp.data < next_node.data:
-                previous = temp
-                temp = temp.next
-
-            if previous:
-                current.next = next_node.next
-                next_node.next = temp
-                previous.next = next_node
-            else:
-                current.next = next_node.next
-                next_node.next = linked_list.head
-                linked_list.head = next_node
-        else:
-            current = next_node
-
-    return linked_list
-
+    # Implement solution here
+    pass
